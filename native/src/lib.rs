59770cd79a5f3753dd113657c481d54ac4eb2a95
Brett Norris <b.norris@calendly.com>
2020-09-29 11:54:03 -0400

    Initial commit

---
+++ native/src/lib.rs
@@ -0,0 +1,11 @@
+use neon::prelude::*;
+
+extern crate num_cpus;
+
+fn thread_count(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    Ok(cx.number(num_cpus::get() as f64))
+}
+
+register_module!(mut cx, {
+    cx.export_function("threadCount", thread_count)
+});
